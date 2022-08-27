c667fb153e631ac759834988a0bd625043032194
vinicius3.98 <vinicius3_1998@hotmail.com>
2022-08-27 12:17:18 -0300

    incluse bcrypt

--- src/users/User.js
+++ src/users/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const useSchema = new mongoose.Schema({
   name: {
@@ -25,6 +26,11 @@ const useSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+useSchema.pre("save", async function (next) {
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
 const User = mongoose.model("User", useSchema);
